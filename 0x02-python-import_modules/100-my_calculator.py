86ca38fe6686d18447d408685b2a9d64709dbb5d
olanrewajuyusuf <olanrewajuyusuf280@gmail.com>
2024-02-22 01:01:05 -0800

    calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,16 +8,20 @@ if __name__ == "__main__":
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         sys.exit(1)
     elif arg[2] == "+":
-        print("{} {} {} = {}".format(arg[1], arg[2], arg[3], add(int(arg[1]),int(arg[3]))))
+        print("{} {} {} = {}".format(arg[1], arg[2], arg[3],
+              add(int(arg[1]), int(arg[3]))))
         sys.exit(0)
     elif arg[2] == "-":
-        print("{} {} {} = {}".format(arg[1], arg[2], arg[3], sub(int(arg[1]),int(arg[3]))))
+        print("{} {} {} = {}".format(arg[1], arg[2], arg[3],
+              sub(int(arg[1]), int(arg[3]))))
         sys.exit(0)
     elif arg[2] == "*":
-        print("{} {} {} = {}".format(arg[1], arg[2], arg[3], mul(int(arg[1]),int(arg[3]))))
+        print("{} {} {} = {}".format(arg[1], arg[2], arg[3],
+              mul(int(arg[1]), int(arg[3]))))
         sys.exit(0)
     elif arg[2] == "/":
-        print("{} {} {} = {}".format(arg[1], arg[2], arg[3], div(int(arg[1]),int(arg[3]))))
+        print("{} {} {} = {}".format(arg[1], arg[2], arg[3],
+              div(int(arg[1]), int(arg[3]))))
         sys.exit(0)
     else:
         print('Unknown operator. Available operators: +, -, * and /')
