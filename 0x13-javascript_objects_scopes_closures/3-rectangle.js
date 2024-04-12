7e349dff51cdee1bc5de14c2b4b2d6fe2bd32920
olanrewajuyusuf <olanrewajuyusuf280@gmail.com>
2024-04-12 07:32:04 -0700

    3-rect

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print() {
+  print () {
     for (let i = 0; i < this.height; i++) {
       let x = "";
       for (let j = 0; j < this.width; j++) {
