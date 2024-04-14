fbdc2b14579202ee02152e934ded242f8f21e528
olanrewajuyusuf <olanrewajuyusuf280@gmail.com>
2024-04-14 05:18:23 -0700

    rev

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 exports.esrever = function (list) {
   let res = [];
   let len = list.length - 1;
-  while (let i = len; i >= 0; i--) {
+  for (let i = len; i >= 0; i--) {
     res.push(list[i]);
   }
   return res;
