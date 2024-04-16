923804c2cc49e2eec6c023fdb3b9bf1d0077170d
olanrewajuyusuf <olanrewajuyusuf280@gmail.com>
2024-04-16 11:37:47 -0700

    concat

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,10 +3,10 @@
 const {writeFile, readFile} = require('fs');
 const args = process.argv;
 
-if (args.length === 5) {
-  const fileA = readFile(args[2], 'utf8');
-  const fileB = readFile(args[3], 'utf8');
+if (args.length === 4) {
+  const fileA = readFile(args[1], 'utf8');
+  const fileB = readFile(args[2], 'utf8');
 
   writeFile(args[4], `${fileA}\n${fileB}`);
-  console.log(readFileSync(args[4], "utf8"));
+  console.log(readFileSync(args[3], "utf8"));
 }
