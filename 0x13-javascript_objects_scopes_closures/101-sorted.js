d33915c4969ef70bd11a42b3c350f3c1aee0f731
olanrewajuyusuf <olanrewajuyusuf280@gmail.com>
2024-04-16 23:42:35 -0700

    sorted

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const dict = require('./101-sorted.js').dict;
+
+const vals = Object.values(dict);
+const keys = Object.keys(dict);
+let res = {};
+for (let i = 0; i < vals.length; i++) {
+ res[vals[i]] = keys.filter(key => dict[key] === vals[i]);
+}
+console.log(res);
