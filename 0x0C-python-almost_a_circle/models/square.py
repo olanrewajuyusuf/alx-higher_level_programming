94887a39b082a4a0add403349e7045e4d038ceae
olanrewajuyusuf <olanrewajuyusuf280@gmail.com>
2024-05-14 00:41:00 -0700

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,9 +7,15 @@ class Square(Rectangle):
     """Representing square"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialize the Square instance."""
+        """Initialize the Square instance.
+
+        Args:
+            size: The size of the square
+        """
+
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     def __str__(self):
         """Return string representation of the Square instance."""
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x, self.y, self.size)
