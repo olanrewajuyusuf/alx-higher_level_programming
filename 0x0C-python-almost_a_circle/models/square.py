b1cda0040177740d348d86b49fa2d6e9d7e0e277
olanrewajuyusuf <olanrewajuyusuf280@gmail.com>
2024-05-14 01:17:04 -0700

    setter/getter

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
 from models.rectangle import Rectangle
 
-"""Creating a square model"""
+"""Craeting Square model"""
 
 class Square(Rectangle):
     """Representing square"""
-
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialize the Square instance.
-
-        Args:
-            size: The size of the square
-        """
+        """Initializing square instances"""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """setter and getter method of size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
     def __str__(self):
-        """Return string representation of the Square instance."""
+        """Returning str representation of square"""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
