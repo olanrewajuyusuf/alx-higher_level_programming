a93b84b8d17e2e91a978109d8b9c384815b8401f
olanrewajuyusuf <olanrewajuyusuf280@gmail.com>
2024-05-22 10:11:19 +0100

    statuscode

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-request
-  .get(process.argv[2])
+request.get(process.argv[2])
   .on('response', function(response) {
     console.log(`code: ${response.statusCode}`);
   });
