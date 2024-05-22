aac4ab5fac0baa46616cc94e98138b1e7735ed14
olanrewajuyusuf <olanrewajuyusuf280@gmail.com>
2024-05-22 09:04:31 +0100

    read_write

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const { readFile } = require('fs');
+
+readFile(process.argv[2], 'utf8', (err, result) => {
+	if (err) {
+		console.log(err);
+		return;
+	}
+	console.log(result)
+})
---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const { writeFile } = require('fs');
+
+const file = process.argv[2];
+const text = process.argv[3];
+
+writeFile(file, text, (err, result) => {
+	if (err) {
+		console.log(err);
+		return;
+	}
+})
