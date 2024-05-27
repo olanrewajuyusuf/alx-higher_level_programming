e04b4be5e769b70b442319191271d6eab80b11b7
olanrewajuyusuf <olanrewajuyusuf280@gmail.com>
2024-05-27 17:48:40 +0100

    starwar

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
 const request = require('request');
+const uri = 'https://swapi-api.alx-tools.com/api/films/:' + process.argv[2];
 
-request('https://swapi-api.alx-tools.com/api/films/:id', function (error, response, body) {
+request(uri, function (error, response, body) {
   console.log(error || JSON.parse(body).title);
 });
