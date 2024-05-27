7de6d56c6db2acc344a29afb3abc7c795e02112e
olanrewajuyusuf <olanrewajuyusuf280@gmail.com>
2024-05-27 18:05:47 +0100

    starwar_count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,8 +4,8 @@ const request = require('request');
 
 request(process.argv[2], function (error, response, body) {
   if (!error) {
-    const res = JSON.parse(body).results;
-    console.log(res.reduce((count, movie) => {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
       return movie.characters.find((character) => character.endsWith('/18/'))
         ? count + 1
 	: count;
